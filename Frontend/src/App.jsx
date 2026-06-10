0aeb48892433ddffe3ed362abfdbe3d13fbb0e40
landeo29 <joslan2906@gmail.com>
2026-06-10 17:40:35 -0500

    chore: setup frontend React con Vite y Tailwind

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600 bg-white px-8 py-4 rounded-xl shadow-lg">
+          Tailwind funciona ✅
+        </h1>
+      </div>
+  )
+}
+
+export default App
